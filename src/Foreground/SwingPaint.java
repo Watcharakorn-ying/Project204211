939534eab227407e81501392f1d6de1bb8df51c8
Nuttawut-S <nuttawut_sukaew@cmu.ac.th>
2017-10-26 18:12:55 +0700

    เปลี่ยนแพ็กเกจ

--- src/Foreground/SwingPaint.java
+++ src/Foreground/SwingPaint.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package drawarea;
+package Foreground;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
