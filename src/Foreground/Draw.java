fea9c8f15e3dec441b4b3c866be0d1f7103cfee2
kawisara-thongdeeloet <Kawisara_th@cmu.ac.th>
2017-11-13 23:16:20 +0700

    Revert "BG"
    This reverts commit b671e817021adc79399fc6a493a61a7b553b40d6.

--- src/Foreground/Draw.java
+++ src/Foreground/Draw.java
@@ -33,7 +33,6 @@ class DrawWin extends JFrame {
     private JComboBox selectNumber;
     private JLabel textNumber;
     private JToggleButton training;
-    private JLabel background;
     
     ActionListener actionListener = new ActionListener() {        
         public void actionPerformed(ActionEvent e) {
@@ -66,7 +65,7 @@ class DrawWin extends JFrame {
         }
         
     };    
-
+    
     public DrawWin() {
         setTitle("Test");
         setSize(new Dimension(1000, 410));
@@ -76,11 +75,6 @@ class DrawWin extends JFrame {
         setLayout(null);
         
         drawPanel = new Draw();
-
-        background = new JLabel();
-        background.setSize(600,410);
-        background.setIcon(new ImageIcon("gui/bg.png"));
-        background.setVisible(true);
         
         selectNumber = new JComboBox();
         selectNumber.setBounds(390,300,60,30);
@@ -162,7 +156,6 @@ class DrawWin extends JFrame {
         getContentPane().add(selectNumber);
         getContentPane().add(textNumber);
         getContentPane().add(training);
-        getContentPane().add(background);
         //getContentPane().add(Draw.repeat);
         //getContentPane().add(Draw.crop);
         setVisible(true);
@@ -240,6 +233,5 @@ public class Draw extends JPanel implements MouseMotionListener, MouseListener {
     
     public static void main(String [] args){
         DrawWin d = new DrawWin();
-        
     }
 }
